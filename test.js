000edd1dd2b4be7e210d465b7dee4ef8a8c9b8ca
Vais Salikhov <vsalikhov@gmail.com>
2021-09-03 13:30:02 -0400

    The First Test

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-describe('Bowling Game', () => {});
+const assert = require('assert').strict;
+const score = require('./index.js');
+
+describe('Bowling Game', () => {
+  let expected;
+  let actual;
+
+  context('all gutter balls', () => {
+    beforeEach(() => {
+      expected = 0;
+      actual = score(Array(20).fill(0));
+    });
+
+    it('scores 0', () => {
+      assert.equal(actual, expected);
+    });
+  });
+});
